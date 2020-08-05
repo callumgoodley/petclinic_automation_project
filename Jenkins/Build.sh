9cdfe29993917239cd72066eed9483ed62257f95
LStokes96 <LStokes@qa.com>
2020-08-05 12:42:01 +0100

    Adding Jenkins

--- Jenkins/Build.sh
+++ Jenkins/Build.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
-
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y unzip wget
+sudo apt install -y wget unzip
 wget https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip
 unzip terraform_*_linux_*.zip
 sudo mv terraform /usr/local/bin
 rm terraform_*_linux_*.zip
 
-az part
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+if cd petclinic_automation_project; then git pull; else git clone https://github.com/callumgoodley/petclinic_automation_project.git; fi
+cd petclinic_automation_project
+git checkout Terraform
 cd Terraform
+cd ScaleSet 
+
 terraform init
 terraform plan
-terraform apply -yes
+terraform apply -auto-approve 
