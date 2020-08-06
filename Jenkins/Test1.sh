2cd0dc265462dae021f1f5ed9b27f8a89101975b
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 17:43:54 +0100

    Update Test1.sh

--- Jenkins/Test1.sh
+++ Jenkins/Test1.sh
@@ -3,7 +3,7 @@ pwd
 sudo apt-get install chromium-browser -y
 export CHROME_BIN=/usr/bin/chromium-browser
 pwd
-cd /home/jenkins/.jenkins/workspace/Project/spring-petclinic-angular
+cd /home/jenkins/.jenkins/workspace/Testing3/spring-petclinic-angular
 sudo rm package-lock.json
 sudo npm install -g n
 sudo n latest
