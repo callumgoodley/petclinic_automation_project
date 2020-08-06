05fd2ce1b1600783630ab92e26261b9c9bbd0580
LStokes96 <65770073+LStokes96@users.noreply.github.com>
2020-08-06 11:42:11 +0100

    Update Build-kube.sh

--- Jenkins/Build-kube.sh
+++ Jenkins/Build-kube.sh
@@ -1,10 +1,3 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y wget unzip
-wget https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip
-unzip terraform_*_linux_*.zip
-sudo mv terraform /usr/local/bin
-rm terraform_*_linux_*.zip
-
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
