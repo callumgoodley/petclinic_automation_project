82a9ea28df76272f8c6b93d28cd6f39ab212019c
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 11:37:22 +0100

    Create install-docker-compose.sh

---
+++ Jenkins/install-docker-compose.sh
@@ -0,0 +1,4 @@
+sudo apt install -y curl jq
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
