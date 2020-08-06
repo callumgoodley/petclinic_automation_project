bc7ab0d184b4799bd9d1128af340eeb786b373df
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 11:46:22 +0100

    Update install-docker-compose.sh

--- Jenkins/install-docker-compose.sh
+++ Jenkins/install-docker-compose.sh
@@ -1,3 +1,8 @@
+sudo apt update
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker $(whoami)
+
+
 sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
