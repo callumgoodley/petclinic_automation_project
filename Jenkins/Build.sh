8ccf8c1b67252a9f71d72a3375c0c0db7768b088
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 11:37:33 +0100

    Update Build.sh

--- Jenkins/Build.sh
+++ Jenkins/Build.sh
@@ -1,8 +1,5 @@
 
-sudo apt install -y curl jq
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 if cd petclinic_automation_project; then git pull; else git clone https://github.com/callumgoodley/petclinic_automation_project.git; fi
