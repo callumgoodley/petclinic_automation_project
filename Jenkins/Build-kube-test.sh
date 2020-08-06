0a63dfa60aec683cdfd08e046efa1a54479b48b3
LStokes96 <65770073+LStokes96@users.noreply.github.com>
2020-08-06 12:14:43 +0100

    Create Build-kube-test.sh

---
+++ Jenkins/Build-kube-test.sh
@@ -0,0 +1,17 @@
+  
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+if cd petclinic_automation_project; then git pull; else git clone https://github.com/callumgoodley/petclinic_automation_project.git; fi
+cd petclinic_automation_project
+git checkout terra-kube
+cd Terraform
+
+terraform init
+terraform plan
+terraform apply -auto-approve 
+sudo az aks install-cli
+sudo az aks get-credentials --resource-group project-resources --name example-aks2
+sudo kubectl get nodes
