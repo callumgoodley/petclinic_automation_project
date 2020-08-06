3bf2e7ab307c40f656acc8c733059f0f8cf7d96d
LStokes96 <65770073+LStokes96@users.noreply.github.com>
2020-08-06 12:53:34 +0100

    Update Build-kube-test.sh

--- Jenkins/Build-kube-test.sh
+++ Jenkins/Build-kube-test.sh
@@ -4,9 +4,9 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
-if cd petclinic_automation_project; then git pull; else git clone https://github.com/callumgoodley/petclinic_automation_project.git; fi
+
 cd petclinic_automation_project
-git checkout terra-kube
+git checkout Testing
 cd Terraform
 cd Test
 
