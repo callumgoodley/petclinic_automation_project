91cf63c52123632d9f44761bc5d87dc586bae56f
LStokes96 <65770073+LStokes96@users.noreply.github.com>
2020-08-06 18:15:21 +0100

    Update Test1.sh

--- Jenkins/Test1.sh
+++ Jenkins/Test1.sh
@@ -4,7 +4,11 @@ sudo apt-get install chromium-browser -y
 export CHROME_BIN=/usr/bin/chromium-browser
 pwd
 cd /home/jenkins/.jenkins/workspace/Testing3/spring-petclinic-angular
+pwd
+ls
 sudo rm package-lock.json
+ls
 sudo npm install -g n
 sudo n latest
+ls
 ng test --watch=false --browsers=ChromeHeadless
