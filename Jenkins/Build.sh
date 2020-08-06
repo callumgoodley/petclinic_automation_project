ae8d449eea965c735e3a0a430b7fcd146d68d385
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 11:37:07 +0100

    Update Build.sh

--- Jenkins/Build.sh
+++ Jenkins/Build.sh
@@ -1,9 +1,3 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y wget unzip
-wget https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip
-unzip terraform_*_linux_*.zip
-sudo mv terraform /usr/local/bin
-rm terraform_*_linux_*.zip
 
 sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
