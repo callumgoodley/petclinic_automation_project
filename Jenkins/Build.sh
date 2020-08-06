096c71b216dd4bea73acc7b2ce878e277d87f233
LStokes96 <65770073+LStokes96@users.noreply.github.com>
2020-08-06 11:31:53 +0100

    Update Build.sh

--- Jenkins/Build.sh
+++ Jenkins/Build.sh
@@ -5,6 +5,10 @@ unzip terraform_*_linux_*.zip
 sudo mv terraform /usr/local/bin
 rm terraform_*_linux_*.zip
 
+sudo apt install -y curl jq
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 if cd petclinic_automation_project; then git pull; else git clone https://github.com/callumgoodley/petclinic_automation_project.git; fi
