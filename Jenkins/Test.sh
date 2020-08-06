766de1b47e0a997236121b0d895c4fe685dbfd88
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 15:12:43 +0100

    Update Test.sh

--- Jenkins/Test.sh
+++ Jenkins/Test.sh
@@ -1,3 +1,5 @@
 export CHROME_BIN=/usr/bin/chromium-browser
 cd /home/jenkins/.jenkins/workspace/Testing3/spring-petclinic-angular
+sudo rm package-lock.json
+sudo npm install
 ng test --watch=false --browsers=ChromeHeadless
