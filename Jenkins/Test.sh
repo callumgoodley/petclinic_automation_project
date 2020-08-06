59fdc35f5f60a879ac6f9a4c3d112eeb7a7f59b1
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-06 12:44:16 +0100

    Update Test.sh

--- Jenkins/Test.sh
+++ Jenkins/Test.sh
@@ -1,2 +1,2 @@
-cd /home/jenkins/.jenkins/workspace/Group/petclinic_automation_project/spring-petclinic-angular
+cd /home/jenkins/.jenkins/workspace/Testing/petclinic_automation_project/spring-petclinic-angular
 ng test --watch=false --browsers=ChromeHeadless
