4a70a64c203fd44fd65524374753f57a9be5c781
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-08-07 09:17:21 +0100

    Update Kubernetes.sh

--- Jenkins/Kubernetes.sh
+++ Jenkins/Kubernetes.sh
@@ -1,5 +1,5 @@
-sudo kubectl apply -f Kubernetes/deployment.yaml
-sudo kubectl apply -f Kubernetes/front-deployment.yaml
-sudo kubectl apply -f Kubernetes/service.yaml
+sudo kubectl apply -f Kubernetes/deployment2.yaml
+sudo kubectl apply -f Kubernetes/front-deployment2.yaml
+sudo kubectl apply -f Kubernetes/service2.yaml
 sleep 20
 sudo kubectl get services
